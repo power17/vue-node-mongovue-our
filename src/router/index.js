7868ceffdbfe25a61d75a02717078d081b83cfe2
power <1410837981@qq.com>
2018-07-24 22:07:59 +0800

    '订单确认的实现'

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import GoodLists from '@/view/GoodLists'
 import Cart from '@/view/Cart'
 import Address from '@/view/Address'
+import OrderConfirm from '@/view/OrderConfirm'
+import OrderSuccess from '@/view/OrderSuccess'
 
 Vue.use(Router)
 
@@ -22,6 +24,14 @@ export default new Router({
       path: '/address',
       name: 'Address',
       component: Address
+    }, {
+      path: '/orderConfirm',
+      name: 'OrderConfirm',
+      component: OrderConfirm
+    },{
+      path: '/orderSuccess',
+      name: 'OrderSuccess',
+      component: OrderSuccess
     }
   ]
 })
